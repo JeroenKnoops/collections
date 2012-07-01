4020d8b77b1d3e07322ec4b3a15ab1fb42671c8e
Jeroen Knoops <jeroen.knoops@gmail.com>
2012-07-01 14:44:39 +0200

    added other secret_token and added it in .gitignore

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Collections::Application.config.secret_token = 'd47a155c83ad88f414d2bef905ed36de0317f4b5717de967bee65a8f46bc6e2334ecab948bcb58abffefe49a750bd42b7be36eeeb74d6747df3a6926255372d7'
+Collections::Application.config.secret_token = 'e6f3fb841ff3ef85c7650e6f3fb841ff3ef85c765011f2e82ff45ea9b3ff811f2e82ff45ea9b3ff8'
